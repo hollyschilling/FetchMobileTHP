4468d3b6527dd65edfbc1560f69d04c85984111c
Holly Schilling <holly.a.schilling@outlook.com>
2024-10-16 07:53:12 -0500

    Initial Commit

---
+++ TakeHomeProject/TakeHomeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeHomeProjectApp.swift
+//  TakeHomeProject
+//
+//  Created by Holly on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct TakeHomeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
